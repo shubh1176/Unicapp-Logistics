0bd2e9eb922725bc605e29d97f0b807bf1032bff
Shubh Raheja <shubh42003@gmail.com>
2024-08-21 20:44:30 +0530

    Added responsiveness in landing page

--- app/page.jsx
+++ app/page.jsx
@@ -36,34 +36,104 @@ export default function Home() {
 
           {/* Responsive Image Loading and Positioning */}
           <div className="absolute top-24 sm:top-56 md:top-44 left-24 sm:left-20 md:left-64 w-10 sm:w-12 md:w-16 lg:w-20 h-10 sm:h-12 md:h-16 lg:h-20 transform -translate-x-1/2 rotate-0">
-            <Image src={'/images/flower.svg'} alt="Flower Bouquet" fill objectFit="contain" priority />
+            <Image 
+              src={'/images/flower.svg'} 
+              alt="Flower Bouquet" 
+              fill 
+              objectFit="contain" 
+              priority 
+              sizes="(max-width: 640px) 50px, (max-width: 768px) 75px, (max-width: 1024px) 100px, 150px"
+            />
           </div>
           <div className="absolute bottom-4 sm:bottom-6 md:bottom-9 right-32 sm:right-48 md:right-96 w-8 sm:w-10 md:w-12 lg:w-16 h-8 sm:h-10 md:h-12 lg:h-16 transform translate-x-1/2 rotate-12">
-            <Image src={'/images/lunchbox.svg'} alt="Lunch Box" fill objectFit="contain" priority />
+            <Image 
+              src={'/images/lunchbox.svg'} 
+              alt="Lunch Box" 
+              fill 
+              objectFit="contain" 
+              priority 
+              sizes="(max-width: 640px) 50px, (max-width: 768px) 75px, (max-width: 1024px) 100px, 150px"
+            />
           </div>
           <div className="absolute bottom-16 sm:bottom-24 md:bottom-44 left-16 sm:left-20 md:left-32 w-10 sm:w-12 md:w-16 lg:w-20 h-10 sm:h-12 md:h-16 lg:h-20 transform -translate-x-1/2 rotate-12">
-            <Image src={'/images/book.svg'} alt="Book" fill objectFit="contain" priority />
+            <Image 
+              src={'/images/book.svg'} 
+              alt="Book" 
+              fill 
+              objectFit="contain" 
+              priority 
+              sizes="(max-width: 640px) 50px, (max-width: 768px) 75px, (max-width: 1024px) 100px, 150px"
+            />
           </div>
           <div className="absolute bottom-6 sm:bottom-8 md:bottom-10 left-48 sm:left-64 md:left-96 w-12 sm:w-16 md:w-20 lg:w-28 h-12 sm:h-16 md:h-20 lg:h-28 transform -translate-x-1/2 rotate-12">
-            <Image src={'/images/charger.svg'} alt="Charger" fill objectFit="contain" priority />
+            <Image 
+              src={'/images/charger.svg'} 
+              alt="Charger" 
+              fill 
+              objectFit="contain" 
+              priority 
+              sizes="(max-width: 640px) 60px, (max-width: 768px) 80px, (max-width: 1024px) 100px, 150px"
+            />
           </div>
           <div className="absolute bottom-16 sm:bottom-20 md:bottom-24 left-24 sm:left-32 md:left-56 w-10 sm:w-12 md:w-16 lg:w-24 h-10 sm:h-12 md:h-16 lg:h-24 transform -translate-x-1/2 rotate-0">
-            <Image src={'/images/drink.svg'} alt="Coffee Cup" fill objectFit="contain" priority />
+            <Image 
+              src={'/images/drink.svg'} 
+              alt="Coffee Cup" 
+              fill 
+              objectFit="contain" 
+              priority 
+              sizes="(max-width: 640px) 60px, (max-width: 768px) 80px, (max-width: 1024px) 100px, 150px"
+            />
           </div>
           <div className="absolute bottom-20 sm:bottom-28 md:bottom-44 left-28 sm:left-40 md:left-80 w-10 sm:w-12 md:w-16 lg:w-20 h-10 sm:h-12 md:h-16 lg:h-20 transform -translate-x-1/2 rotate-0">
-            <Image src={'/images/Medicines1.svg'} alt="Medicines" fill objectFit="contain" priority />
+            <Image 
+              src={'/images/Medicines1.svg'} 
+              alt="Medicines" 
+              fill 
+              objectFit="contain" 
+              priority 
+              sizes="(max-width: 640px) 60px, (max-width: 768px) 80px, (max-width: 1024px) 100px, 150px"
+            />
           </div>
           <div className="absolute top-20 sm:top-24 md:top-48 right-24 sm:right-32 md:right-64 w-10 sm:w-16 md:w-20 lg:w-28 h-10 sm:h-16 md:h-20 lg:h-28 transform translate-x-1/2 rotate-4">
-            <Image src={'/images/dress.svg'} alt="Dress" fill objectFit="contain" priority />
+            <Image 
+              src={'/images/dress.svg'} 
+              alt="Dress" 
+              fill 
+              objectFit="contain" 
+              priority 
+              sizes="(max-width: 640px) 60px, (max-width: 768px) 100px, (max-width: 1024px) 150px, 200px"
+            />
           </div>
           <div className="absolute top-32 sm:top-40 md:top-60 right-16 sm:right-20 md:right-32 w-10 sm:w-12 md:w-16 lg:w-24 h-10 sm:h-12 md:h-16 lg:h-24 transform translate-x-1/2 rotate-0">
-            <Image src={'/images/key.svg'} alt="Key" fill objectFit="contain" priority />
+            <Image 
+              src={'/images/key.svg'} 
+              alt="Key" 
+              fill 
+              objectFit="contain" 
+              priority 
+              sizes="(max-width: 640px) 60px, (max-width: 768px) 80px, (max-width: 1024px) 100px, 150px"
+            />
           </div>
           <div className="absolute bottom-16 sm:bottom-20 md:bottom-28 right-24 sm:right-32 md:right-80 w-10 sm:w-12 md:w-16 lg:w-24 h-10 sm:h-12 md:h-16 lg:h-24 transform translate-x-1/2 rotate-0">
-            <Image src={'/images/grocery.svg'} alt="Grocery Basket" fill objectFit="contain" priority />
+            <Image 
+              src={'/images/grocery.svg'} 
+              alt="Grocery Basket" 
+              fill 
+              objectFit="contain" 
+              priority 
+              sizes="(max-width: 640px) 60px, (max-width: 768px) 80px, (max-width: 1024px) 100px, 150px"
+            />
           </div>
           <div className="absolute top-60 sm:top-80 md:top-96 right-8 sm:right-12 md:right-24 w-8 sm:w-10 md:w-12 lg:w-16 h-8 sm:h-10 md:h-12 lg:h-16 transform translate-x-1/2 rotate-0">
-            <Image src={'/images/envelop.svg'} alt="Envelope" fill objectFit="contain" priority />
+            <Image 
+              src={'/images/envelop.svg'} 
+              alt="Envelope" 
+              fill 
+              objectFit="contain" 
+              priority 
+              sizes="(max-width: 640px) 50px, (max-width: 768px) 60px, (max-width: 1024px) 80px, 100px"
+            />
           </div>
 
           <div className="mt-6 md:mt-10 hover:cursor-pointer">
@@ -98,6 +168,7 @@ export default function Home() {
             className="h-auto w-auto sm:h-[400px] sm:w-[450px] md:h-[600px] md:w-[650px] lg:h-[850px] lg:w-[1050px]" 
             priority={true} 
             alt="Sticker"
+            sizes="(max-width: 640px) 300px, (max-width: 768px) 450px, (max-width: 1024px) 650px, 1050px"
           />
           
           {/* Responsive positioning for the icon */}
@@ -109,6 +180,7 @@ export default function Home() {
               className="h-auto w-auto sm:h-[40px] sm:w-[45px] md:h-[60px] md:w-[70px] lg:h-[90px] lg:w-[90px]" 
               priority={true} 
               alt="Icon"
+              sizes="(max-width: 640px) 30px, (max-width: 768px) 45px, (max-width: 1024px) 70px, 90px"
             />
           </div>
         </div>
@@ -122,7 +194,14 @@ export default function Home() {
           <span className="font-filson text-[#000000] text-3xl md:text-5xl"> it?</span>
         </div>
         <span className="font-generalRegular mb-5 md:mb-10 text-lg md:text-2xl mt-2 md:mt-5">Don't worry, it's easy ;)</span>
-        <Image src={'/images/howtouse.svg'} height={450} width={500} className="md:h-[900px] md:w-[1100px]" loading="lazy" />
+        <Image 
+          src={'/images/howtouse.svg'} 
+          height={450} 
+          width={500} 
+          className="md:h-[900px] md:w-[1100px]" 
+          loading="lazy" 
+          sizes="(max-width: 640px) 450px, (max-width: 1024px) 900px, 1100px"
+        />
       </div>
 
       <div className="mt-10 md:mt-20 mb-10">
