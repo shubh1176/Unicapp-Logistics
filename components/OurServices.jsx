9b031136badf515c0727bc17d28743790e22e0a8
Shubh Raheja <shubh42003@gmail.com>
2024-08-21 20:33:55 +0530

    Added responsiveness in landing page

--- components/OurServices.jsx
+++ components/OurServices.jsx
@@ -32,32 +32,33 @@ function OurServices() {
         </div>
 
         {/* Service Cards for Small Screens */}
-        <div className='grid grid-cols-2 gap-4 md:hidden mb-8 mt-8 mx-4 md:mx-8 md:px-12 lg:px-56 transition-all duration-300 ease-in-out'>
+        <div className='grid grid-cols-2 gap-3 md:hidden mb-6 mt-6 mx-2 md:mx-8 md:px-12 lg:px-56 transition-all duration-300 ease-in-out'>
           <div className='flex flex-col items-center transition-all duration-300 ease-in-out'>
-            <div className='bg-[#F8EBAB] h-44 w-44 md:h-60 md:w-60 rounded-2xl flex justify-center items-center transition-all duration-300 ease-in-out'>
-              <Image src={'/images/groupimage.svg'} height={160} width={160} alt="Pickup & Drop" />
+            <div className='bg-[#F8EBAB] h-36 w-36 md:h-60 md:w-60 rounded-2xl flex justify-center items-center transition-all duration-300 ease-in-out'>
+              <Image src={'/images/groupimage.svg'} height={140} width={140} alt="Pickup & Drop" />
             </div>
-            <div className='w-44 md:w-60 mt-2 hover:cursor-pointer transition-all duration-300 ease-in-out' onClick={() => router.replace('/Pickup-and-drop')}>
+            <div className='w-36 md:w-60 mt-1 hover:cursor-pointer transition-all duration-300 ease-in-out' onClick={() => router.replace('/Pickup-and-drop')}>
               <span className='flex flex-row justify-between bg-black rounded-2xl p-1'>
                 <span className='font-generalRegular text-white py-2 px-2 space-x-1 text-sm'>
                   Pickup & Drop
                 </span>
-                <Button className="rounded-full bg-[#9E3CE1] border-spacing-1 p-2 hover:bg-[#9E3CE1] transition-all duration-300 ease-in-out">
+                <Button className="rounded-full bg-[#9E3CE1] border-spacing-1 p-1 hover:bg-[#9E3CE1] transition-all duration-300 ease-in-out">
                   <LucideArrowUpRight />
                 </Button>
               </span>
             </div>
           </div>
+          
           <div className='flex flex-col items-center transition-all duration-300 ease-in-out'>
-            <div className='bg-[#F8EBAB] h-44 w-44 md:h-60 md:w-60 rounded-2xl flex justify-center items-center transition-all duration-300 ease-in-out'>
+            <div className='bg-[#F8EBAB] h-36 w-36 md:h-60 md:w-60 rounded-2xl flex justify-center items-center transition-all duration-300 ease-in-out'>
               <Image src={'/images/delivery.svg'} height={48} width={120} alt="Intercity Courier" />
             </div>
-            <div className='w-44 md:w-60 mt-2 hover:cursor-pointer transition-all duration-300 ease-in-out' onClick={() => router.replace('/courier')}>
+            <div className='w-36 md:w-60 mt-1 hover:cursor-pointer transition-all duration-300 ease-in-out' onClick={() => router.replace('/courier')}>
               <span className='flex flex-row justify-between bg-black rounded-2xl p-1'>
                 <span className='font-generalRegular text-white py-2 px-2 space-x-1 text-sm'>
                   Intercity Courier
                 </span>
-                <Button className="rounded-full bg-[#9E3CE1] border-spacing-1 p-2 hover:bg-[#9E3CE1] transition-all duration-300 ease-in-out" onClick={() => router.replace('/courier')}>
+                <Button className="rounded-full bg-[#9E3CE1] border-spacing-1 p-1 hover:bg-[#9E3CE1] transition-all duration-300 ease-in-out" onClick={() => router.replace('/courier')}>
                   <LucideArrowUpRight />
                 </Button>
               </span>
