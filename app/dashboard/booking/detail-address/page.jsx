2e7cdef96cc1db17d2d651ceffda16f42f31c5b1
Shubh Raheja <shubh42003@gmail.com>
2024-08-22 13:16:41 +0530

    added responsiveness

--- app/dashboard/booking/detail-address/page.jsx
+++ app/dashboard/booking/detail-address/page.jsx
@@ -5,10 +5,10 @@ import RightDivAdd from './_components/RightDivAdd';
 function Page() {
   return (
     <div className="flex flex-col lg:flex-row h-auto lg:h-full bg-white ">
-      <div className="lg:w-1/2 w-full lg:h-full pt-4 pb-20 bg-white lg:-translate-y-5">
+      <div className="lg:w-1/2 w-full lg:h-full pt-2 pb-20 px-6 bg-white lg:-translate-y-5">
         <LeftDivAdd />
       </div>
-      <div className="lg:w-1/2 w-full flex justify-center lg:justify-start pl-10 bg-white -translate-y-48">
+      <div className="lg:w-1/2 w-full flex justify-center lg:justify-start pl-10 bg-white -translate-y-52">
         <RightDivAdd />
       </div>
     </div>
