f2a66ccb2cc011a2e397ee008a2270c98c32d7a0
Shubh Raheja <shubh42003@gmail.com>
2024-08-23 14:51:41 +0530

    added responsiveness

--- app/dashboard/booking/checkout/page.jsx
+++ app/dashboard/booking/checkout/page.jsx
@@ -2,14 +2,13 @@ import React from 'react';
 import Checkout from './_components/Checkout';
 import Details from './_components/Details';
 
-
 function Page() {
   return (
-    <div className="flex flex-col lg:flex-row h-auto lg:h-full bg-white">
-      <div className="lg:w-1/2 w-full lg:h-full pt-2 sm:pb-28 px-6 bg-white lg:translate-y-0 lg:mr-8">
+    <div className="flex flex-col lg:flex-row h-auto lg:h-full bg-white px-4 sm:px-6 lg:px-10 py-6 lg:py-10 gap-6 lg:gap-8">
+      <div className="lg:w-1/2 w-full lg:h-full pt-2 bg-white lg:translate-y-0">
         <Details />
       </div>
-      <div className="lg:w-1/2 w-full flex justify-center lg:justify-start px-6 lg:px-10 bg-white pb-10">
+      <div className="lg:w-1/2 w-full flex justify-center lg:justify-start bg-white">
         <Checkout />
       </div>
     </div>
