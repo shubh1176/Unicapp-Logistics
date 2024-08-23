524bab57eaf9d997972a83f034e7cd298c0bb474
Shubh Raheja <shubh42003@gmail.com>
2024-08-23 23:47:31 +0530

    added responsiveness

--- app/dashboard/booking/confirmation/_components/RightDiv5.jsx
+++ app/dashboard/booking/confirmation/_components/RightDiv5.jsx
@@ -8,18 +8,18 @@ import React from 'react'
 function RightDiv5() {
     const router = useRouter()
   return (
-    <div>
-      <div className='flex flex-col gap-7 mt-52 ml-28'>
-        <div className='flex flex-row gap-4'>
+    <div className="flex justify-center items-center h-full"> {/* Ensure RightDiv5 is centered on the screen */}
+      <div className='flex flex-col gap-7 mt-0 lg:mt-20 lg:ml-20 text-center lg:text-left'> {/* Adjust margin for small screens */}
+        <div className='flex flex-col lg:flex-row gap-4 items-center'>
             <Image src={'/images/Approval.svg'} width={100} height={100} />
-            <h2 className='font-generalSemiBold mt-8 text-4xl'>Order Confirmed!</h2>
+            <h2 className='font-generalSemiBold mt-4 lg:mt-8 text-4xl'>Order Confirmed!</h2>
         </div>
-        <div className='translate-x-4'>
+        <div className='lg:translate-x-4'>
             <span className='text-base font-generalMedium'>
             Now sit back and relax! We'll assign a delivery <br></br> Capptain to your specified time. <br /><br /> Tracking details will be on their way as soon<br /> as your order is dispatched. You can check <br /> them in My Profile section
             </span>
         </div>
-        <div className='w-60 translate-x-4 mt-4' onClick={()=>{router.push('/dashboard')}}>
+        <div className='w-60 lg:translate-x-4 mt-4 mx-auto lg:mx-0' onClick={()=>{router.push('/dashboard')}}>
             <div className='flex flex-row justify-between border-2 border-zinc-950 rounded-xl py-1 px-6'>
                 <span className='font-generalMedium mt-2'>
                     Go to My Profile
@@ -34,4 +34,4 @@ function RightDiv5() {
   )
 }
 
-export default RightDiv5
+export default RightDiv5;
--- app/dashboard/booking/confirmation/page.jsx
+++ app/dashboard/booking/confirmation/page.jsx
@@ -5,10 +5,12 @@ import RightDiv5 from './_components/RightDiv5';
 function Page() {
   return (
     <div className="flex flex-col lg:flex-row h-auto lg:h-full bg-white">
-      <div className="lg:w-1/2 w-full lg:h-full pt-2 sm:pb-28 px-6 bg-white lg:translate-y-0 lg:mr-8">
+      {/* Hide LeftDiv5 on small screens and show it on large screens */}
+      <div className="hidden lg:block lg:w-1/2 lg:h-full pt-2 sm:pb-28 px-6 bg-white lg:translate-y-0 lg:mr-8">
         <LeftDiv5 />
       </div>
-      <div className="lg:w-1/2 w-full flex justify-center lg:justify-start px-6 lg:px-10 bg-white pb-10">
+      {/* RightDiv5 takes full width on small screens and half width on large screens */}
+      <div className="lg:w-1/2 w-full h-screen flex items-center justify-center px-6 lg:px-10 bg-white">
         <RightDiv5 />
       </div>
     </div>
