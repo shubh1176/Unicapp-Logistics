c531e6f205e24b6f8d74cae81331072df741da3f
Shubh Raheja <shubh42003@gmail.com>
2024-08-23 20:54:01 +0530

    added responsiveness

--- app/dashboard/booking/confirmation/page.jsx
+++ app/dashboard/booking/confirmation/page.jsx
@@ -4,12 +4,12 @@ import RightDiv5 from './_components/RightDiv5';
 
 function Page() {
   return (
-    <div className="flex flex-col lg:flex-row h-auto lg:h-full bg-white">
+    <div className="flex flex-col lg:flex-row h-screen lg:h-full bg-white">
       {/* Hide LeftDiv5 on small screens and show it on large screens */}
       <div className="hidden lg:block lg:w-1/2 lg:h-full pt-2 sm:pb-28 px-6 bg-white lg:translate-y-0 lg:mr-8">
         <LeftDiv5 />
       </div>
-      <div className="lg:w-1/2 w-full h-screen lg:h-full flex items-center justify-center px-6 lg:px-10 lg:pt-32 bg-white">
+      <div className="lg:w-1/2 w-full h-full flex items-center justify-center px-6 lg:px-10 bg-white">
         <RightDiv5 />
       </div>
     </div>
