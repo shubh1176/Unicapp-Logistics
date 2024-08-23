43cb796b05d92aa658a21cb8cfa61a660057f771
Shubh Raheja <shubh42003@gmail.com>
2024-08-23 23:34:00 +0530

    added responsiveness

--- app/dashboard/booking/confirmation/page.jsx
+++ app/dashboard/booking/confirmation/page.jsx
@@ -9,7 +9,7 @@ function Page() {
       <div className="hidden lg:block lg:w-1/2 lg:h-full pt-2 sm:pb-28 px-6 bg-white lg:translate-y-0 lg:mr-8">
         <LeftDiv5 />
       </div>
-      <div className="lg:w-1/2 w-full h-screen lg:h-full flex items-center justify-center px-4 sm:px-6 lg:px-10 lg:pt-32 bg-white">
+      <div className="lg:w-1/2 w-full h-screen lg:h-full flex items-center justify-center px-6 lg:px-10 lg:pt-32 bg-white">
         <RightDiv5 />
       </div>
     </div>
@@ -17,4 +17,3 @@ function Page() {
 }
 
 export default Page;
-
