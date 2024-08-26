dbdfb2a5ea17abb3bb46e2f3b1a0c4695e6f1c50
Shubh Raheja <shubh42003@gmail.com>
2024-08-26 16:16:39 +0530

    added responsiveness

--- app/dashboard/booking/checkout/page.jsx
+++ app/dashboard/booking/checkout/page.jsx
@@ -8,7 +8,7 @@ function Page() {
       <div className="lg:w-1/2 w-full lg:h-full pt-2 sm:pb-28 px-6 lg:pt-0 lg:mr-8 -mt-2">
         <Details />
       </div>
-      <div className="lg:w-1/2 w-full flex justify-center lg:justify-start px-6 lg:px-12 pb-10 lg:pb-0 lg:items-center lg:-translate-y-48 lg:ml-8">
+      <div className="lg:w-1/2 w-full flex justify-center lg:justify-start px-6 lg:px-12 pb-10 lg:pb-0 lg:items-center lg:-translate-y-56 lg:ml-8">
         <Checkout />
       </div>
     </div>
