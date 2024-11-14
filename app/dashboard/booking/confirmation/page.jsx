6028335d46a60a1d3860575c807b2e5aff33c643
Junaid Arif <mohdjunaid7078@gmail.com>
2024-11-14 22:13:26 +0530

    fix confirmation screen in Booking on Desktop

--- app/dashboard/booking/confirmation/page.jsx
+++ app/dashboard/booking/confirmation/page.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import LeftDiv5 from './_components/LeftDiv5';
 import RightDiv5 from './_components/RightDiv5';
+import BookingLeftDiv from '@/components/BookingLeftDiv';
 
 function Page() {
   return (
-    <div className="flex flex-col gap-4 lg:flex-row h-auto lg:h-full bg-[#F1EDEA]">
-      <div className="lg:w-1/2 w-full lg:h-full pt-2 sm:pb-28 px-6 lg:pt-0 lg:mr-8 -mt-2">
-        <LeftDiv5 />
+    <div className="flex flex-col lg:gap-4 lg:flex-row h-auto lg:h-full bg-[#F1EDEA] lg:overflow-hidden">
+      <div className="lg:w-1/2 w-full md:w-1/2   lg:pb-0 lg:px-6 lg:pt-0 lg:mr-8 ">
+        <BookingLeftDiv />
       </div>
-      <div className="lg:w-1/2 w-full flex justify-center lg:justify-start px-6 lg:px-12 pb-10 lg:pb-0 lg:items-center lg:-translate-y-60 lg:translate-x-12 lg:ml-8">
+      <div className="lg:w-1/2 w-full flex justify-center lg:justify-start px-6 lg:px-10 pb-10 lg:pb-0   lg:translate-x-20  lg:overflow-y-scroll    ">
         <RightDiv5 />
       </div>
     </div>
